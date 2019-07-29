1836ea7c3e39ba8cb785a0225efa0f761cc96a72
Shantay Grady <shantaygrady@gmail.com>
2019-07-29 00:35:31 -0400

    Creates hooks and params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from './components/DisplayComponents/Display';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -12,6 +15,14 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+const [displayValue, setDispplayValue] = useState('0');
+const updateValue = param => {
+  setDispplayValue(displayValue === '0' ? param : displayValue + param);
+}
+const resetValue = () => setDisplayValue('0');
+const calculateValue = () => {
+  setDisplayValue(eval(displayValue));
+}
 
   return (
     <div className="container">
