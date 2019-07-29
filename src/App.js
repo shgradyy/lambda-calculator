a3cc5c3989b82d8a3bdf97a1b99132380dd83039
Shantay Grady <shantaygrady@gmail.com>
2019-07-29 01:17:00 -0400

    fixes state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
+
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 import Display from './components/DisplayComponents/Display';
@@ -15,20 +17,34 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-const [displayValue, setDispplayValue] = useState('0');
-const updateValue = param => {
-  setDispplayValue(displayValue === '0' ? param : displayValue + param);
-}
-const resetValue = () => setDisplayValue('0');
-const calculateValue = () => {
-  setDisplayValue(eval(displayValue));
-}
+  const [displayValue, setDisplayValue] = useState('0');
+
+  const updateValue = param => {
+    setDisplayValue(displayValue === '0' ? param : displayValue + param);
+  }
+
+  const resetValue = () => setDisplayValue('0');
+
+  const calculateValue = () => {
+    setDisplayValue(eval(displayValue));
+  }
 
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Logo />
+        <Display displayValue={displayValue} />
+        <div className="buttons-container">
+          <div>
+            <Specials updateValue={updateValue} resetValue={resetValue} />
+            <Numbers updateValue={updateValue} />
+          </div>
+          <div>
+            <Operators updateValue={updateValue} calculateValue={calculateValue} />
+          </div>
+        </div>
       </div>
     </div>
   );
